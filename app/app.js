a1d38cac55de39a26b4f82bb5fa7224178eb6247
Alireza <alirezahusseini33@gmail.com>
2021-01-24 16:20:41 +0300

    new changes

--- app/app.js
+++ app/app.js
@@ -1,8 +1,16 @@
 const navMenu = document.querySelector(".nav-menu");
 const humbergerMenu = document.querySelector(".humberger-menu");
+const navLink = document.querySelectorAll(".nav-link");
 
 // event letseners
 humbergerMenu.addEventListener("click", () => {
     navMenu.classList.toggle("open");
     humbergerMenu.classList.toggle("animate");
+});
+
+// closing mobile-nav when clicking the links
+navLink.forEach((link) => {
+    link.addEventListener("click", () => {
+        link.classList.remove("open");
+    });
 });
