e672db879ed332ad83e5dadb0991b94418e664da
Alireza <alirezahusseini33@gmail.com>
2021-01-25 00:26:27 +0300

    'portfolio'

--- app/app.js
+++ app/app.js
@@ -2,6 +2,8 @@ const navMenu = document.querySelector(".nav-menu");
 const humbergerMenu = document.querySelector(".humberger-menu");
 const navLink = document.querySelectorAll(".nav-link");
 const navLists = document.querySelectorAll("li");
+const nav = document.querySelector("nav");
+const humbergerLine = document.querySelectorAll(".lines");
 // event letseners
 humbergerMenu.addEventListener("click", () => {
     navMenu.classList.toggle("open");
@@ -18,4 +20,34 @@ navLists.forEach((link) => {
             navMenu.classList.remove("open");
         }, 100);
     });
+});
+
+// navbar background color
+window.onscroll = function() {
+    let top = window.scrollY;
+    if (top >= 5) {
+        nav.classList.add("nav-bg");
+        // humbergerLine.forEach((line) => {
+        //     line.classList.add("line-light");
+        // });
+    } else {
+        nav.classList.remove("nav-bg");
+        // humbergerLine.forEach((line) => {
+        //     line.classList.remove("line-light");
+        // });
+    }
+};
+
+// mouse hover effect on portfolio section
+const leftSide = document.querySelector(".design");
+const rightSide = document.querySelector(".frond-end");
+
+const leftSideContainer = document.querySelector(".container-1");
+const rightSideContainer = document.querySelector(".container-2");
+
+leftSideContainer.addEventListener("mouseover", () => {
+    document.querySelector(".frond-end").style.zIndex = "-1";
+    setTimeout((e) => {
+        document.querySelector(".frond-end").style.zIndex = "1";
+    }, 2000);
 });
